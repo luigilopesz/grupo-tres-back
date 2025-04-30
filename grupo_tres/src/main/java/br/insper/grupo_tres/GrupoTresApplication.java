98bbb12bf52f748d82642bb71835b01104f99b88
luigilopesz <luigilopes09@gmail.com>
2025-04-30 11:10:43 -0300

    db connection

---
+++ grupo_tres/src/main/java/br/insper/grupo_tres/GrupoTresApplication.java
@@ -0,0 +1,13 @@
+package br.insper.grupo_tres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoTresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoTresApplication.class, args);
+	}
+
+}
